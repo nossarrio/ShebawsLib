e94b6b3435e7da89f0eb4f5c0c66bc82f38b4f89
Amadasun Nosa <nossarrio@yahoo.com>
2021-04-05 18:46:37 +0000

    Initial Commit
    Initial Commit

---
+++ Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ShebawsCoreLibrary
+{
+    public class ShebawsService
+    {
+        string Url = "";       
+
+        public ShebawsService(string url)
+        {
+            Url = url;
+        }
+
+        public string getObservation()
+        {
+            return Utility.HTTPGet(Url);
+        } 
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ShebawsCoreLibrary
+{
+    public static class Utility
+    {
+
+        public static string HTTPGet(string Url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.Accept = "application/json";
+            request.ContentType = "application/json";
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
